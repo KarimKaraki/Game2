a110e8341e40b2759f87a178038cd5520ee1308a
HekmatK <113850158+HekmatK@users.noreply.github.com>
2022-11-05 01:50:11 +0200

    Update Sprite.java

--- src/Sprite.java
+++ src/Sprite.java
@@ -9,7 +9,6 @@ import java.awt.Image;
  * lots of different places without having to store multiple 
  * copies of the image.
  * 
- * @author Kevin Glass
  */
 public class Sprite {
 	/** The image to be drawn for this sprite */
